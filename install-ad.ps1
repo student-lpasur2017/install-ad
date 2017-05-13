3c781745d63cd36c17c1b2cb23e553543af874ad
Baptiste Fotia <Baptiste Fotia>
2017-05-13 08:44:55 +0200

    Premier commit avec juste install service AD

---
+++ install-ad.ps1
@@ -0,0 +1,5 @@
+# Affichage des interfaces avec leurs index
+Get-NetAdapter
+
+# Installation du service AD
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
