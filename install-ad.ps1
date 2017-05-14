41fa1bfee0605239fe2da416389bdf4c1434d753
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-14 18:50:39 +0200

    Ajout deploiement d'une premiere foret facon statique

--- install-ad.ps1
+++ install-ad.ps1
@@ -3,3 +3,10 @@ Get-NetAdapter
 
 # Installation du service AD
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
+
+#---------- Deploiement d'une nouvelle foret ----------
+
+# Import module pour avoir les cmdelts de conf pour AD
+Import-Module ADDSDeployment
+# /!\ Commande a tester et a la rendre dynamique si possible
+Install-ADDSForest -CreateDnsDelegation False -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "mycompany.com" -DomainNetbiosName "MYCOMPANY" -Forest%ode "Win2012R2" -InstallDns True -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion False -SysvolPath "C:\Windows\SYSVOL" -Force True
