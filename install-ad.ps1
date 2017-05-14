94b0314a1145d6267cd7e642cd942576e333356b
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-05-14 21:47:00 +0200

    Ligne 24: Conf dynamique AD

--- install-ad.ps1
+++ install-ad.ps1
@@ -1,3 +1,7 @@
+# Initialisation des variables
+$NameNetBios=""
+$namedomaine=""
+
 # Affichage des interfaces avec leurs index
 Get-NetAdapter
 
@@ -8,5 +12,13 @@ Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Include
 
 # Import module pour avoir les cmdelts de conf pour AD
 Import-Module ADDSDeployment
+
+# Lire variables
+$namedomaine=Read-Host -Prompt "Quel est le nom de domaine (entre crochet) "
+$NameNetBios=Read-Host -Prompt "Quel est le nom du Net Bios (entre crochet, tout en majuscule et sans TLD) "
+
 # /!\ Commande a tester et a la rendre dynamique si possible
-Install-ADDSForest -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "mycompany.com" -DomainNetbiosName "MYCOMPANY" -Forestmode "Win2012R2" -InstallDns True -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion False -SysvolPath "C:\Windows\SYSVOL" -Force True
+# Statique
+# Install-ADDSForest -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName "mycompany.com" -DomainNetbiosName "MYCOMPANY" -Forestmode "Win2012R2" -InstallDns True -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion False -SysvolPath "C:\Windows\SYSVOL" -Force True
+# Conf AD Dynamique
+Install-ADDSForest -DatabasePath "C:\Windows\NTDS" -DomainMode "Win2012R2" -DomainName $namedomain -DomainNetbiosName $NameNetBios -Forestmode "Win2012R2" -InstallDns True -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion False -SysvolPath "C:\Windows\SYSVOL" -Force True
